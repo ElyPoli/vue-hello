b3e50b9983c4bc07bd698b3d5a89dc58a1f63678
ElyPoli <itsos.poli@gmail.com>
2023-07-19 15:42:51 +0200

    added vue message

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 "use strict";
 
+// Creo la funzione createApp
+const app = Vue.createApp({
+    data() {
+        return {
+            pageTitle: "Vue Hello",
+            classTitle: "text-center text-white fw-bold text-uppercase pt-5 pb-4",
+            imageUrl: "https://picsum.photos/600/400",
+            imageAlt: "Immagine random",
+            classImg: "border rounded-4",
+        }
+    }
+})
+
+// Attivo vue
+app.mount("#app");
